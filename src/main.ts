88e7d311f8dd8eed0e06a8334c2814e82b6d641b
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2023-10-17 00:40:02 +0600

    Disabled CORS checks for hostname in development

--- src/main.ts
+++ src/main.ts
@@ -53,10 +53,7 @@ async function bootstrap() {
 
     app.useStaticAssets(config.get<string>('MEDIA_DEST') || 'public');
 
-    app.enableCors({
-      origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
-      credentials: true,
-    });
+    app.enableCors();
   }
 
   const port = parseInt(config.get('PORT', '8000'));
