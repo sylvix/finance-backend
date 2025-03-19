150f88d8eff21c7c31c5ba0b77b1055ca8c138af
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2025-03-19 23:51:38 +0600

    Updated dependencies

--- src/auth/jwtRefresh.strategy.ts
+++ src/auth/jwtRefresh.strategy.ts
@@ -18,7 +18,7 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwtRefresh')
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('REFRESH_TOKEN_SECRET'),
+      secretOrKey: configService.get<string>('REFRESH_TOKEN_SECRET') || '',
       passReqToCallback: true,
     });
   }
